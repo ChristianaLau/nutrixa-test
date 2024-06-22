220307cba7e4b86c3299a16a0791673732cda9f9
ChristianaLau <CHRISTIANA.LAU25@myhunter.cuny.edu>
2024-06-22 18:40:06 -0400

    fixes

--- nutrixa-auth/middleware.js
+++ nutrixa-auth/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/profile', '/register',"api/webhooks/clerk"],
+  publicRoutes: ['/', '/profile', '/register',"/api/webhooks/clerk"],
 });
 
 export const config = {
